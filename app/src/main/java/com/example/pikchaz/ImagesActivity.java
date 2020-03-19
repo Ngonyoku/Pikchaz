51bdcdb590fa19e6342238956a70125a78038b88
Ngonyoku <ngonyoku001@gmail.com>
2020-03-20 00:28:04 +0300

    initial Commitment

---
+++ app/src/main/java/com/example/pikchaz/ImagesActivity.java
@@ -0,0 +1,104 @@
+package com.example.pikchaz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.List;
+
+public class ImagesActivity extends AppCompatActivity implements ImageAdapter.OnItemClickListener {
+    private RecyclerView mRecyclerView;
+    private ImageAdapter mAdapter;
+    private ProgressBar mProgressCircle;
+    private FirebaseStorage mStorage;
+    private DatabaseReference mDatabaseRef;
+    private EventListener mDBListener;
+    private List<Upload> mUploads;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mProgressCircle = findViewById(R.id.progress_circle);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mUploads = new ArrayList<>();
+        mAdapter = new ImageAdapter(ImagesActivity.this,mUploads);
+        mRecyclerView.setAdapter(mAdapter);
+        mStorage = FirebaseStorage.getInstance();
+        mAdapter.setOnItemClickListener(ImagesActivity.this);
+        mDatabaseRef = mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+        //To get data out of mUploads
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUploads.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    upload.setKey(postSnapshot.getKey());
+                    mUploads.add(upload);
+                }
+                mAdapter.notifyDataSetChanged();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            //Used when displaying Errors
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ImagesActivity.this, databaseError.getMessage(), Toast.LENGTH_LONG).show();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+
+            }
+        });
+    }
+
+    @Override
+    public void OnItemClick(int position) {
+        Toast.makeText(this, "Normal Click at position : " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onWhatEverClick(int position) {
+        Toast.makeText(this, "Whatever click at position : " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDeleteClick(int position) {
+        Upload selectedItem = mUploads.get(position);
+        final String selectedKey = selectedItem.getKey();
+
+        StorageReference imageRef = mStorage.getReferenceFromUrl(selectedItem.getImageUrl());
+        imageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                mDatabaseRef.child(selectedKey).removeValue();
+                Toast.makeText(ImagesActivity.this, "Item Deleted Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mDatabaseRef.removeEventListener((ValueEventListener) mDBListener);
+    }
+}
